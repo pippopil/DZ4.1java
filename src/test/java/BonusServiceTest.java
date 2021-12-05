a33990e64aee8a7ec52d29b462961b3be2d2cf7a
Nikolay Abadeev <voood234@gmail.com>
2021-12-05 15:46:48 +0300

    Add

---
+++ src/test/java/BonusServiceTest.java
@@ -0,0 +1,62 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BonusServiceTest {
+
+    @Test
+    void shoudCalculateRegistredAndBonusUnderLimit() {
+        BonusService service = new BonusService();
+
+        long amount = 1000_60;
+        boolean registred = true;
+        long expected = 30;
+
+        long actual = service.calculate(amount, registred);
+
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shoudCalculateRegistredAndBonusOverLimit() {
+        BonusService service = new BonusService();
+
+        long amount = 1000_000_60;
+        boolean registred = true;
+        long expected = 500;
+
+        long actual = service.calculate(amount, registred);
+
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shoudCalculateNoRegistredAndBonusUnderLimit() {
+        BonusService service = new BonusService();
+
+        long amount = 1000_60;
+        boolean registred = false;
+        long expected = 10;
+
+        long actual = service.calculate(amount, registred);
+
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shoudCalculateNRegistredAndBonusUnderLimit() {
+        BonusService service = new BonusService();
+
+        long amount = 1000_000_000_60L;
+        boolean registred = false;
+        long expected = 500;
+
+        long actual = service.calculate(amount, registred);
+
+
+        assertEquals(expected, actual);
+    }
+}
